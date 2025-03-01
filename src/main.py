d8db4f350b3f9b20590d88a2459248c66a6fd951
wilzeviolin <93179639+wilzeviolin@users.noreply.github.com>
2025-03-01 13:25:52 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,13 +4,27 @@ from roanne_carapp import roanne_app
 
 main_app = Flask(__name__, template_folder='../templates')
 
-# Register Blueprints
-main_app.register_blueprint(wileen_app, url_prefix='/wheat')
-main_app.register_blueprint(roanne_app, url_prefix='/car')
-
+# Register Wileen Routes
 @main_app.route('/')
 def home():
     return "Welcome to the Merged Flask App 🔥"
+
+@main_app.route('/wheat')
+def wheat():
+    return wileen_app.view_functions['home_page']()
+
+@main_app.route('/wheat/process', methods=['POST'])
+def wheat_process():
+    return wileen_app.view_functions['process_form']()
+
+# Register Roanne Routes
+@main_app.route('/car')
+def car():
+    return roanne_app.view_functions['car_page']()
+
+@main_app.route('/car/predict', methods=['POST'])
+def car_predict():
+    return roanne_app.view_functions['predict']()
 
 if __name__ == '__main__':
     main_app.run(host='0.0.0.0', port=5000, debug=True)

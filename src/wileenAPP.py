d677cf9ff920433d93545d77e910d5db56f0b7cf
wilzeviolin <93179639+wilzeviolin@users.noreply.github.com>
2025-03-01 13:57:50 +0800

    Update wileenAPP.py

--- src/wileenAPP.py
+++ src/wileenAPP.py
@@ -67,3 +67,5 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
     print(f"Starting Flask app on port {port}")
     app.run(host='0.0.0.0', port=port, debug=False)
+
+app=app
